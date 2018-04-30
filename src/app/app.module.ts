30f4eab741f3ae23192b3b3e1fb234f9a1e42fd5
Tales Marinho Godois <tales.godois@gmail.com>
2018-04-30 11:56:05 -0300

    Why: Inserir animações do angular
    *
    Insere  imports
    
    *

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
